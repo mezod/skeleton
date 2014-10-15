85054b770c26afe8f1e2f83cec27059b857e1111
mezod <mezood@gmail.com>
2014-10-15 02:12:08 +0200

    installing require and marionette

---
+++ app/scripts/main.js
@@ -0,0 +1,28 @@
+require.config({
+  paths: {
+    'jquery': 'vendor/jquery/dist/jquery',
+    'underscore': 'vendor/underscore/underscore',
+    'backbone': 'vendor/backbone/backbone',
+    'backbone.babysitter': 'vendor/backbone.babysitter/lib/backbone.babysitter.js',
+    'backbone.wreqr': 'vendor/backbone.wreqr/lib/backbone.wreqr.js',
+    'backbone.marionette': 'vendor/backbone.marionette/lib/core/backbone.marionette.js',
+  },
+  shim: {
+  underscore: {
+    exports: '_'
+  },
+  backbone: {
+    exports: 'Backbone',
+    deps: ['jquery', 'underscore']
+  },
+  marionette: {
+    exports: 'Backbone.Marionette',
+    deps: ['backbone']
+  }
+  },
+  deps: ['jquery', 'underscore']
+});
+
+require(['views/app'], function(AppView) {
+  new AppView;
+});
